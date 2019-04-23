522df0aafcb73fed1428c9ec85af42cf96ac12ab
Dillon <dtidwell123@gmail.com>
2019-04-23 18:07:49 -0400

    Portfolio Round 2

---
+++ README.md
@@ -0,0 +1,5 @@
+## Designed by Dillon
+My personal portfolio site made and deployed using React and Netlify. 
+Use NPM Install if you'd like to run on your personal machine.
+Any comments or questions feel free to contact me, thanks.
+
---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"http://github.com/ByDillon",
+    "name": "Dillon Tidwell",
+    "role": "Full-Stack Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/dillon-tidwell/",
+    "skypeid": "NoSkype",
+    "roleDescription": " I enjoy automation, learning and Node.JS.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dillon-tidwell/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/ByDillon",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/ByDillon",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a student/graduate from the UCF Full Stack Web Development Bootcamp. I enjoy working with people to solve problems. Currently I am a full stack developer working on websites for clients as well as personal projects, such as NightBeat. I enjoy many hobbies from cars, watches, computers (hardware), and almost anything with moving parts! My goal is to get into the DevOps field.",
+    "address":"Central FL",
+    "website":"https://designedbydillon.com",
+    "education":[
+      {
+        "UniversityName":"UCF Full Stack Bootcamp",
+        "specialization":"Web Development",
+        "MonthOfPassing":"Graduated May",
+        "YearOfPassing":"2019",
+        "Achievements":"designedbydillon.com, forum.treadathletics.com, getarnow.com, forum.theairlineacademy.careers, INSTACrypto @ ByDillon.github.io/groupproject1/"
+      },
+      {
+        "UniversityName":"Daytona State => FIU",
+        "specialization":"Business",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2016-2018",
+        "Achievements":"Attended DSC Starting 2016, Transferred to FIU Fall of 2017-Spring 2018"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Publix",
+        "specialization":"Front Service Clerk, Bakery Clerk, Produce Clerk",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2017",
+        "Achievements":"Cross-Trained in 3 departments, led the best cookie sale the store has seen."
+      },
+      {
+        "CompanyName":"Sunfest Media",
+        "specialization":"Social Media Marketing",
+        "MonthOfLeaving":"Contract",
+        "YearOfLeaving":"Work",
+        "Achievements":"Admin of various Local-Business Facebook pages, forum.theairlineacademy.com"
+      }
+    ],
+    "skillsDescription":"Showcase",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"NightBeat",
+        "description":"Co-Developer of Nightbeat.io, a cloud based program to automate checking-out on Shopify (and more..)",
+        "imgurl":"images/portfolio/nightbeat.jpg"
+      },
+      {
+        "name":"GetARnow.com",
+        "description":"GetARnow is a project based off of AR.js, put onto the browser and made marketable",
+        "imgurl":"images/portfolio/getarnow.jpg"
+      },
+      {
+        "name":"forum.treadathletics.com",
+        "description":"Forum made from Discourse on Digital Ocean",  
+        "imgurl":"images/portfolio/tread.jpg"
+      },
+      {
+        "name":"InstaCrypto",
+        "description":"InstaCrypto is a group project I worked on. It features API calls to give real-time useful Crypto related data, as well as a SlackBot",
+        "imgurl":"images/portfolio/instacrypto.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"I enjoy working with people to solve problems, click the second circle to get in contact with me",
+        "name":"Dillon Tidwell"
+      },
+      {
+        "description":"Contact me at the number below!",
+        "name":"386-314-5241"
+      }
+    ]
+  }
+  
+  export default resumeData
